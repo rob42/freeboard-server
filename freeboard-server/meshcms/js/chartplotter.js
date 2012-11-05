0f3538f37a37161d8a6c18b3c06b3676eb0f4ab0
robert <robert@42.co.nz>
2012-11-05 20:01:45 +1300

    Moved chartplotter javascript to meshcms/js dir Split out map data to layers.js

--- freeboard-server/meshcms/js/chartplotter.js
+++ freeboard-server/meshcms/js/chartplotter.js
@@ -2,9 +2,7 @@
 // var mapBounds = new OpenLayers.Bounds( 173.014971899, -41.1579399972,
 // 173.061684157, -41.1303857203);
 // var mapMinZoom = 12;
-var mapBounds = new OpenLayers.Bounds(171.672658537, -41.3148906329, 174.54758838, -40.5453691267);
-var mapMinZoom = 0;
-var mapMaxZoom = 18;
+
 var chartProjection = new OpenLayers.Projection("EPSG:900913");
 var screenProjection = new OpenLayers.Projection("EPSG:4326");
 var shipMarker = new OpenLayers.Layer.Vector('Ship', {
@@ -33,118 +31,9 @@ function init() {
 	};
 	map = new OpenLayers.Map('map', options);
 
-	// create TMS Overlay layer
-
-	// INSERT_HERE
-	var NZ6151_1 = new OpenLayers.Layer.TMS("NZ6151_1 ",
-			"../../mapcache/NZ6151_1/", {
-				layername : '../../mapcache/NZ6151_1/',
-				type : 'png',
-				getURL : overlay_getTileURL,
-				alpha : true,
-				isBaseLayer : false,
-				numZoomLevels : 18,
-				minZoomLevel : 10,
-				maxZoomLevel : 15,
-				buffer : 1
-			});
-
-	var NZ615 = new OpenLayers.Layer.TMS("NZ615 ", "../../mapcache/NZ615/", {
-		layername : '../../mapcache/NZ615/',
-		type : 'png',
-		getURL : overlay_getTileURL,
-		alpha : true,
-		isBaseLayer : false,
-		numZoomLevels : 18,
-		minZoomLevel : 8,
-		maxZoomLevel : 13,
-		buffer : 1
-	});
-
-	var NZ61 = new OpenLayers.Layer.TMS("NZ61 ", "../../mapcache/NZ61/", {
-		layername : '../../mapcache/NZ61/',
-		type : 'png',
-		getURL : overlay_getTileURL,
-		alpha : true,
-		isBaseLayer : true,
-		numZoomLevels : 18,
-		minZoomLevel : 7,
-		maxZoomLevel : 12,
-		buffer : 1
-	});
-
-	var NZ614 = new OpenLayers.Layer.TMS("NZ614 Tasman Bay",
-			"../../mapcache/NZ614/", { // url: '', serviceVersion: '.',
-				layername : '../../mapcache/NZ614/',
-				type : 'png',
-				getURL : overlay_getTileURL,
-				alpha : true,
-				isBaseLayer : false,
-				numZoomLevels : 18,
-				// maxResolution: "auto",
-				minZoomLevel : 8,
-				maxZoomLevel : 13,
-				buffer : 1
-			});
-	var NZ6144 = new OpenLayers.Layer.TMS("NZ6144 Abel Tasman",
-			"../../mapcache/NZ6144/", { // url: '', serviceVersion: '.',
-				layername : '../../mapcache/NZ6144/',
-				type : 'png',
-				getURL : overlay_getTileURL,
-				alpha : true,
-				isBaseLayer : false,
-				// numZoomLevels: 18 ,
-				maxResolution : "auto",
-				minZoomLevel : 9,
-				maxZoomLevel : 15,
-				buffer : 1
-			});
-	var NZ6142_1 = new OpenLayers.Layer.TMS("NZ6142_1 Nelson Harbour",
-			"../../mapcache/NZ6142_1/", { // url: '', serviceVersion: '.',
-				layername : '../../mapcache/NZ6142_1/',
-				type : 'png',
-				getURL : overlay_getTileURL,
-				alpha : true,
-				isBaseLayer : false,
-				// numZoomLevels: 18 ,
-				maxResolution : "auto",
-				minZoomLevel : 10,
-				maxZoomLevel : 15,
-				buffer : 1
-			});
-	var NZ6142_2 = new OpenLayers.Layer.TMS("NZ6142_2 Port Nelson",
-			"../../mapcache/NZ6142_2/", { // url: '', serviceVersion: '.',
-				layername : '../../mapcache/NZ6142_2/',
-				type : 'png',
-				getURL : overlay_getTileURL,
-				alpha : true,
-				isBaseLayer : false,
-				// numZoomLevels: 18 ,
-				maxResolution : "auto",
-				minZoomLevel : 12,
-				maxZoomLevel : 17,
-				buffer : 1
-			});
-	// create TMS Overlay layer
-	var NZ614_1 = new OpenLayers.Layer.TMS("NZ614_1 Port Motueka",
-			"../../mapcache/NZ614_1/", { // url: '', serviceVersion: '.',
-				layername : '../../mapcache/NZ614_1/',
-				type : 'png',
-				getURL : overlay_getTileURL,
-				alpha : true,
-				isBaseLayer : false,
-				// numZoomLevels: 18 ,
-				maxResolution : "auto",
-				minZoomLevel : 12,
-				maxZoomLevel : 18,
-				buffer : 1
-			});
-	// if (OpenLayers.Util.alphaHack() == false) { tmsoverlay.setOpacity(0.7); }
+	//add layers
+	addLayers(map);
 	
-
-	map.addLayers([ NZ61,NZ614, NZ6142_1, NZ6142_2, shipMarker]);
-	//map.addLayers([ NZ61, NZ614, NZ6142_1, NZ6142_2, NZ6144, NZ614_1, NZ615,NZ6151_1, vector ]);
-
 	var switcherControl = new OpenLayers.Control.LayerSwitcher();
 	map.addControl(switcherControl);
 	switcherControl.maximizeControl();
