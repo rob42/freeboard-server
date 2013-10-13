e3947bf1b38b4834d2d443f37ac36a4d52bd06ff
robert <robert@42.co.nz>
2013-10-14 09:46:03 +1300

    Added startpc.bat so freeboard-server can run directly on windows pc's

---
+++ startpc.bat
@@ -0,0 +1,30 @@
+
+REM. start script for freeboard on windows pc
+REM. Assume we are starting in the freeboard directory
+REM.set FREEBOARD_HOME=C:\freeboard
+
+set FREEBOARD_HOME=%CD%
+
+set JAR=freeboard-server-SNAPSHOT-jar-with-dependencies.jar
+REM.
+cd %FREEBOARD_HOME%
+
+REM.start server
+set JAVA=java
+
+REM. You may need to set the java version spcifically
+REM. If so uncomment and edit the following to suit your install
+REM. set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_07
+REM. set JAVA=%JAVA_HOME%\bin\java
+
+set EXT="-Djava.util.Arrays.useLegacyMergeSort=true"
+
+REM. optionally limit memory here
+REM. set MEM="-Xmx24m -XX:PermSize=32m -XX:MaxPermSize=32m"
+
+set LOG4J=-Dlog4j.configuration=file:/%FREEBOARD_HOME%/conf/log4j.properties
+
+echo "Starting: %JAVA% %EXT% %LOG4J% %MEM% -jar target/%JAR%" >logs\start.log 2>&1 
+"%JAVA%" %EXT% %LOG4J% %MEM% -jar target\%JAR% >>logs\start.log 2>&1 
+
+pause 
