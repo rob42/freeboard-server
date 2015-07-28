092349394ae44f9cad3ff500f120ac836e307b01
Ronald R. Berliner <RBerliner@InstrumentationAssociates.com>
2015-07-28 19:15:50 -0400

    Fixing some drrors introduced in the last merge.

--- freeboard/js/layers.js
+++ freeboard/js/layers.js
@@ -3,7 +3,7 @@ function addLayers(map) {
 
 	var WORLD = L.tileLayer("http://{server}:8080/mapcache/WORLD/{z}/{x}/{y}.png", {
 		server: host,
-		
+
 		attribution: 'Natural Earth',
 		minZoom: 0,
 		maxZoom: 4,
@@ -13,7 +13,7 @@ function addLayers(map) {
 
 	var WORLD1 = L.tileLayer("http://{server}:8080/mapcache/WORLD1/{z}/{x}/{y}.png", {
 		server: host,
-		
+
 		attribution: 'Natural Earth 1',
 		minZoom: 3,
 		maxZoom: 6,
@@ -21,9 +21,9 @@ function addLayers(map) {
 		}).addTo(map);
 
 
-	var _1552_1 = L.tileLayer("http://{server}:8080/mapcache/11552_1/{z}/{x}/{y}.png", {
+	var _11552_1 = L.tileLayer("http://{server}:8080/mapcache/11552_1/{z}/{x}/{y}.png", {
 		server: host,
-		
+
 		attribution: '11552_1 NEUSE RIVER AND UPPER PART OF BAY RIVER ',
 		minZoom: 10,
 		maxZoom: 15,
@@ -31,9 +31,9 @@ function addLayers(map) {
 		}).addTo(map);
 
 
-	var _1552_2 = L.tileLayer("http://{server}:8080/mapcache/11552_2/{z}/{x}/{y}.png", {
+	var _11552_2 = L.tileLayer("http://{server}:8080/mapcache/11552_2/{z}/{x}/{y}.png", {
 		server: host,
-		
+
 		attribution: 'NZ23 New Zeland North Island',
 		minZoom: 6,
 		maxZoom: 9,
@@ -41,8 +41,9 @@ function addLayers(map) {
 		}).addTo(map);
 
 
+	var _11552_2 = L.tileLayer("http://{server}:8080/mapcache/11552_2/{z}/{x}/{y}.png", {
 		server: host,
-		
+
 		attribution: '11552_2 CONTINUATION OF TRENT RIVER HAYWARD CREE',
 		minZoom: 12,
 		maxZoom: 15,
@@ -50,9 +51,9 @@ function addLayers(map) {
 		}).addTo(map);
 
 
-	var _1552_3 = L.tileLayer("http://{server}:8080/mapcache/11552_3/{z}/{x}/{y}.png", {
+	var _11552_3 = L.tileLayer("http://{server}:8080/mapcache/11552_3/{z}/{x}/{y}.png", {
 		server: host,
-		
+
 		attribution: '11552_3 CONTINUATION OF TRENT RIVER POLLOCKSVILL',
 		minZoom: 12,
 		maxZoom: 15,
@@ -60,9 +61,9 @@ function addLayers(map) {
 		}).addTo(map);
 
 
-	var _1552_4 = L.tileLayer("http://{server}:8080/mapcache/11552_4/{z}/{x}/{y}.png", {
+	var _11552_4 = L.tileLayer("http://{server}:8080/mapcache/11552_4/{z}/{x}/{y}.png", {
 		server: host,
-		
+
 		attribution: '11552_4 CONTINUATION OF NEUSE RIVER NU/518 RA/41',
 		minZoom: 15,
 		maxZoom: 18,
@@ -75,10 +76,10 @@ function addLayers(map) {
 	overlays = {
 		"Natural Earth": WORLD,
 		"Natural Earth 1": WORLD1,
-		"11552_1 NEUSE RIVER AND UPPER PART OF BAY RIVER ": _1552_1,
-		"11552_2 CONTINUATION OF TRENT RIVER HAYWARD CREE": _1552_2,
-		"11552_3 CONTINUATION OF TRENT RIVER POLLOCKSVILL": _1552_3,
-		"11552_4 CONTINUATION OF NEUSE RIVER NU/518 RA/41": _1552_4,
+		"11552_1 NEUSE RIVER AND UPPER PART OF BAY RIVER ": _11552_1,
+		"11552_2 CONTINUATION OF TRENT RIVER HAYWARD CREE": _11552_2,
+		"11552_3 CONTINUATION OF TRENT RIVER POLLOCKSVILL": _11552_3,
+		"11552_4 CONTINUATION OF NEUSE RIVER NU/518 RA/41": _11552_4,
 	};
 	layers = L.control.layers(baseLayers, overlays).addTo(map);
 	};
--- install_rpi.sh
+++ install_rpi.sh
@@ -2,6 +2,7 @@
 #
 # Copyright 2012,2013 Robert Huitema robert@42.co.nz
 # 
+#
 # This file is part of FreeBoard.
 #
 #  FreeBoard is free software: you can redistribute it and/or modify
@@ -57,16 +58,36 @@ sudo update-alternatives --install /usr/bin/java java /opt/jdk1.8.0/bin/java 1
 ###################
 # extract the freeboard-server archive
 #FREEBOARD_CURRENT=freeboard-server-0.5.12-SNAPSHOT
+echo "Extract the freeboard-server archive"
+#FREEBOARD_CURRENT=freeboard-server-0.5.15-SNAPSHOT
 FREEBOARD_CURRENT=$(find -maxdepth 1 -type f -name 'freeboard-server-*-SNAPSHOT-all.zip'| head -n1)
+echo "#FREEBOARD_CURRENT = "$FREEBOARD_CURRENT
 unzip $FREEBOARD_CURRENT
+file_name=${FREEBOARD_CURRENT##*/}
+dir_name=${file_name%-all.*}
+echo "#    Directory name = "$dir_name
 
 #copy the freeboard directory back to here
 cp -rf $FREEBOARD_CURRENT/freeboard .
+cp -rf $dir_name/freeboard .
+
+cd freeboard
+echo "#   Strip out the Win line endings on all *.sh and *.bat files"
+echo "#   *.sh files"
+find ./ -maxdepth 1 -name '*.sh' -exec echo "{}" \;
+find ./ -maxdepth 1 -name '*.sh' -exec sed -i -e 's/\r$//' {} \;
+echo "#   *.bat files"
+find ./ -maxdepth 1 -name '*.bat' -exec echo "{}" \;
+find ./ -maxdepth 1 -name '*.bat' -exec sed -i -e 's/\r$//' {} \;
+
+cd ..
+
 #we need the logs directory
 mkdir freeboard/logs
 
 ###################
 #Install autostart
+echo "#   Install autostart"
 #copy and rename the file to /etc/init.d - you must do this as root hence 'sudo'
 sudo cp -rf ./freeboard/freeboard.init.d.sh /etc/init.d/freeboard
 # change owner and permissions
@@ -76,14 +97,17 @@ sudo chown root:root /etc/init.d/freeboard
 sudo update-rc.d freeboard defaults
 
 # stop the apache server, its in the way
+echo "#   Stop the apache server, its in the way"
 sudo update-rc.d -f apache2 remove
 sudo update-rc.d -f udhcpd remove
 
 ##################
 #Optional: if you are having problems exit here
 #Setup networking
+echo "#   Setup networking"
 #/etc/network/interfaces
 if ! grep -Fq "#Run as wireless access point" /etc/network/interfaces 
+if ! grep -Fq "#Run as wireless access point" /etc/network/interfaces
 then
     sudo sh -c 'echo "" >> /etc/network/interfaces'
     sudo sh -c 'echo "#Run as wireless access point" >> /etc/network/interfaces'
@@ -93,17 +117,20 @@ then
     sudo sh -c 'echo "    netmask 255.255.255.0" >> /etc/network/interfaces'
     sudo sh -c 'echo "    gateway 192.168.0.1" >> /etc/network/interfaces'
 fi
+echo "#   Setup hostname"
 #/etc/hostname
 sudo hostname freeboard
 sudo sh -c 'echo "freeboard" > /etc/hostname'
 
 #/etc/hosts
+echo "#   Setup /etc/hosts"
 sudo sh -c 'echo "# www.zkoss.org is in here to speed the web interface when there is no internet connection" > /etc/hosts'
 sudo sh -c 'echo "127.0.0.1       localhost www.zkoss.org" >> /etc/hosts'
 sudo sh -c 'echo "# Note: the a.freeboard, b.freeboard, etc entries are used by the Leaflet mapping lib to speed up map loading, you only need them if you are using the chartplotter." >> /etc/hosts'
 sudo sh -c 'echo "192.168.0.1	freeboard a.freeboard b.freeboard c.freeboard d.freeboard" >> /etc/hosts'
 
 #dnsmasq
+echo "#   Setup dnsmasq"
 # Configuration file for dnsmasq.
 sudo sh -c 'echo "interface=wlan0" > /etc/dnsmasq.conf'
 sudo sh -c 'echo "dhcp-range=192.168.0.10,192.168.0.128,12h" >> /etc/dnsmasq.conf'
@@ -115,11 +142,13 @@ sudo sh -c 'echo "" >> /etc/dnsmasq.conf'
 
 ##################
 #set up wifi
+echo "#   Set up wifi"
 
 sudo mkdir /etc/hostapd
 sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
 
 # write hostapd.conf
+echo "#   Write hostapd.conf"
 sudo sh -c 'echo "interface=wlan0" > /etc/hostapd/hostapd.conf'
 sudo sh -c 'echo "driver=nl80211" >> /etc/hostapd/hostapd.conf'
 sudo sh -c 'echo "ctrl_interface=/var/run/hostapd" >> /etc/hostapd/hostapd.conf'
@@ -139,4 +168,5 @@ sudo sh -c 'echo "auth_algs=3" >> /etc/hostapd/hostapd.conf'
 sudo sh -c 'echo "wmm_enabled=1" >> /etc/hostapd/hostapd.conf'
 
 #/etc/default/hostapd
+echo "#   Setup /etc/default/hostapd"
 sudo sh -c 'echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" > /etc/default/hostapd'
