e886560bc95ed9a1acf260e53fe7f1d591ee3fe7
RBerliner <RBerliner@InstrumentationAssociates.com>
2016-03-21 14:28:41 -0400

    Redoing commit of install_rpi.sh. Somehow the merge was not done properly.

--- install_rpi.sh
+++ install_rpi.sh
@@ -2,6 +2,7 @@
 #
 # Copyright 2012,2013 Robert Huitema robert@42.co.nz
 # 
+#
 # This file is part of FreeBoard.
 #
 #  FreeBoard is free software: you can redistribute it and/or modify
@@ -50,6 +51,32 @@ sudo apt-get install wpasupplicant usbutils wireless-tools iw hostapd
 
 ###################
 
+# extract the freeboard-server archive
+#FREEBOARD_CURRENT=freeboard-server-0.5.12-SNAPSHOT
+echo "Extract the freeboard-server archive"
+#FREEBOARD_CURRENT=freeboard-server-0.5.15-SNAPSHOT
+FREEBOARD_CURRENT=$(find -maxdepth 1 -type f -name 'freeboard-server-*-SNAPSHOT-all.zip'| head -n1)
+echo "#FREEBOARD_CURRENT = "$FREEBOARD_CURRENT
+unzip $FREEBOARD_CURRENT
+file_name=${FREEBOARD_CURRENT##*/}
+dir_name=${file_name%-all.*}
+echo "#    Directory name = "$dir_name
+
+#copy the freeboard directory back to here
+#cp -rf $FREEBOARD_CURRENT/freeboard .
+cp -rf $dir_name/freeboard .
+
+cd freeboard
+echo "#   Strip out the Win line endings on all *.sh and *.bat files"
+echo "#   *.sh files"
+find ./ -maxdepth 1 -name '*.sh' -exec echo "{}" \;
+find ./ -maxdepth 1 -name '*.sh' -exec sed -i -e 's/\r$//' {} \;
+echo "#   *.bat files"
+find ./ -maxdepth 1 -name '*.bat' -exec echo "{}" \;
+find ./ -maxdepth 1 -name '*.bat' -exec sed -i -e 's/\r$//' {} \;
+
+cd ..
+
 
 #we need the logs directory
 echo "Make sure we have a logs dir.."
@@ -57,6 +84,7 @@ mkdir logs
 
 ###################
 #Install autostart
+echo "#   Install autostart"
 #copy and rename the file to /etc/init.d - you must do this as root hence 'sudo'
 echo "Install autostart for freeboard.."
 sudo cp -rf ./freeboard.init.d.sh /etc/init.d/freeboard
@@ -68,6 +96,7 @@ sudo update-rc.d freeboard defaults
 
 # stop the apache server, its in the way
 
+echo "#   Stop the apache server, its in the way"
 
 sudo update-rc.d -f apache2 remove
 sudo update-rc.d -f udhcpd remove
@@ -75,6 +104,8 @@ sudo update-rc.d -f udhcpd remove
 ##################
 #Optional: if you are having problems exit here
 
+#Setup networking
+echo "#   Setup networking"
 
 #/etc/network/interfaces
 if ! grep -Fq "#Run as wireless access point" /etc/network/interfaces 
@@ -87,18 +118,21 @@ then
     sudo sh -c 'echo "    netmask 255.255.255.0" >> /etc/network/interfaces'
     sudo sh -c 'echo "    gateway 192.168.0.1" >> /etc/network/interfaces'
 fi
+echo "#   Setup hostname"
 #/etc/hostname
 echo "Set hostname to freeboard, and adjust hosts..."
 sudo hostname freeboard
 sudo sh -c 'echo "freeboard" > /etc/hostname'
 
 #/etc/hosts
+echo "#   Setup /etc/hosts"
 sudo sh -c 'echo "# www.zkoss.org is in here to speed the web interface when there is no internet connection" > /etc/hosts'
 sudo sh -c 'echo "127.0.0.1       localhost www.zkoss.org" >> /etc/hosts'
 sudo sh -c 'echo "# Note: the a.freeboard, b.freeboard, etc entries are used by the Leaflet mapping lib to speed up map loading, you only need them if you are using the chartplotter." >> /etc/hosts'
 sudo sh -c 'echo "192.168.0.1	freeboard a.freeboard b.freeboard c.freeboard d.freeboard" >> /etc/hosts'
 
 #dnsmasq
+echo "#   Setup dnsmasq"
 # Configuration file for dnsmasq.
 echo "Setup dnsmasq.."
 sudo sh -c 'echo "interface=wlan0" > /etc/dnsmasq.conf'
@@ -112,10 +146,13 @@ sudo sh -c 'echo "" >> /etc/dnsmasq.conf'
 ##################
 #set up wifi
 
+echo "#   Set up wifi"
+
 sudo mkdir /etc/hostapd
 sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
 
 # write hostapd.conf
+echo "#   Write hostapd.conf"
 sudo sh -c 'echo "interface=wlan0" > /etc/hostapd/hostapd.conf'
 sudo sh -c 'echo "driver=nl80211" >> /etc/hostapd/hostapd.conf'
 sudo sh -c 'echo "ctrl_interface=/var/run/hostapd" >> /etc/hostapd/hostapd.conf'
@@ -136,3 +173,5 @@ sudo sh -c 'echo "wmm_enabled=1" >> /etc/hostapd/hostapd.conf'
 
 #/etc/default/hostapd
 
+echo "#   Setup /etc/default/hostapd"
+sudo sh -c 'echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" > /etc/default/hostapd'
